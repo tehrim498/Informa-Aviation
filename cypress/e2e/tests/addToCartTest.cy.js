8f4aabcd052f8eae695a741cd95e2d037ba504c1
tehrim498 <tehrimansari06@gmail.com>
2025-01-30 00:08:39 +0530

    second testing

---
+++ cypress/e2e/tests/addToCartTest.cy.js
@@ -0,0 +1,17 @@
+import { homePage } from '../../Pages/homePage'
+const homePageObj = new homePage()
+import testData from '../../fixtures/testData.json'
+
+
+describe(' test automation', () => {
+    before(() => {
+        cy.login(testData.login.username, testData.login.password)
+    })
+
+
+    it('Add To Cart flow', () => {
+        homePageObj.searchProduct(testData.product.productName)
+        homePageObj.addToCart()
+        homePageObj.verifySucessMessage().should('contain', testData.message.successMessage).and('contain', testData.product.productName);
+    })
+})
